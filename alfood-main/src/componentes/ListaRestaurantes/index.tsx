77c39c1dd7fb043868f966bf25d5b6eb39178c64
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-09-01 20:22:12 -0300

    first commit

---
+++ alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import IRestaurante from '../../interfaces/IRestaurante';
+import style from './ListaRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+
+const ListaRestaurantes = () => {
+
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+
+  useEffect(() => { 
+    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    .then(response => {
+      setRestaurantes(response.data.results)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [ ])
+
+  return (<section className={style.ListaRestaurantes}>
+    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+  </section>)
+}
+
+export default ListaRestaurantes
