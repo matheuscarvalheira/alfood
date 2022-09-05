8968c7a26ea0d9094effd1ad5367e767340fcb8f
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-09-05 15:49:17 -0300

    creating route for admin's

--- alfood-main/src/App.tsx
+++ alfood-main/src/App.tsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import VitrineRestaurantes from './pages/VitrineRestaurantes';
+import { Routes, Route } from "react-router-dom";
+import AdministracaoRestaurantes from "./pages/Administracao/Restaurantes/AdministracaoRestaurantes";
+import Home from "./pages/Home";
+import VitrineRestaurantes from "./pages/VitrineRestaurantes";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route
+        path="/admin/restaurantes"
+        element={<AdministracaoRestaurantes />}
+      />
     </Routes>
   );
 }
