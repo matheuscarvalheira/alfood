0650021e9ad2892e40080af78c994f614c15eabf
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-09-06 15:12:28 -0300

    creating previous and next page

--- alfood-main/src/components/ListaRestaurantes/index.tsx
+++ alfood-main/src/components/ListaRestaurantes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { IPaginacao } from "../../interfaces/IPaginacao";
@@ -8,32 +9,26 @@ import Restaurante from "./Restaurante";
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState("");
+  const [paginaAnterior, setPaginaAnterior] = useState("");
 
-  useEffect(() => {
+  const carregarDados = (url: string) => {
     axios
-      .get<IPaginacao<IRestaurante>>(
-        "http://localhost:8000/api/v1/restaurantes/"
-      )
+      .get<IPaginacao<IRestaurante>>(url)
       .then((response) => {
         setRestaurantes(response.data.results);
         setProximaPagina(response.data.next);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  const verMais = () => {
-    axios
-      .get<IPaginacao<IRestaurante>>(proximaPagina)
-      .then((response) => {
-        setRestaurantes([...restaurantes, ...response.data.results]);
-        setProximaPagina(response.data.next);
+        setPaginaAnterior(response.data.previous);
+        console.log(response.data)
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
+  useEffect(() => {
+    //obter os restaurantes
+    carregarDados("http://localhost:8000/api/v1/restaurantes/");
+  }, []);
 
   return (
     <section className={style.ListaRestaurantes}>
@@ -43,7 +38,26 @@ const ListaRestaurantes = () => {
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
-      {proximaPagina && <button onClick={verMais}>Ver mais</button>}
+      {
+        <Button
+          onClick={() => {
+            carregarDados(paginaAnterior);
+          }}
+          variant="contained"
+        >
+          Página Anterior
+        </Button>
+      }
+      {
+        <Button
+          onClick={() => {
+            carregarDados(proximaPagina);
+          }}
+          variant="contained"
+        >
+          Próxima página
+        </Button>
+      }
     </section>
   );
 };
