d0b5dd849c343317bb4b10626e6e8acc89ecb5f6
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-09-08 21:52:13 -0300

    creating a link to edit

--- alfood-main/src/pages/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ alfood-main/src/pages/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -9,6 +9,7 @@ import {
 } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import IRestaurante from "../../../interfaces/IRestaurante";
 
 const AdministracaoRestaurantes = () => {
@@ -28,12 +29,16 @@ const AdministracaoRestaurantes = () => {
         <TableHead>
           <TableRow>
             <TableCell>Nome</TableCell>
+            <TableCell>Editar</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {restaurantes.map((restaurante) => (
             <TableRow key= {restaurante.id}>
               <TableCell>{restaurante.nome}</TableCell>
+              <TableCell>
+                [ <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link> ]
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
