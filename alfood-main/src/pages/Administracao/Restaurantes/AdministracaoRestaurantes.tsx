c2def887d50c6f72c03651e62f96b933d4263052
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-09-15 10:49:49 -0300

    putting a toolbar to the admin area

--- alfood-main/src/pages/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ alfood-main/src/pages/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -7,11 +7,18 @@ import {
   TableHead,
   TableRow,
   Button,
+  AppBar,
+  Box,
+  Container,
+  Toolbar,
+  Typography,
+  Link,
 } from "@mui/material";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import http from "../../../http";
 import IRestaurante from "../../../interfaces/IRestaurante";
+
+import { Link as RouterLink } from "react-router-dom";
 
 const AdministracaoRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
@@ -32,38 +39,70 @@ const AdministracaoRestaurantes = () => {
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Nome</TableCell>
-            <TableCell>Editar</TableCell>
-            <TableCell>Excluir</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {restaurantes.map((restaurante) => (
-            <TableRow key={restaurante.id}>
-              <TableCell>{restaurante.nome}</TableCell>
-              <TableCell>
-                [{" "}
-                <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link>{" "}
-                ]
-              </TableCell>
-              <TableCell>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  onClick={() => excluir(restaurante)}
-                >
-                  Excluir
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Typography variant="h6">Administração</Typography>
+            <Box sx={{ display: "flex", flexGrow: 1 }}>
+              <Link component={RouterLink} to="/admin/restaurantes">
+                <Button sx={{ my: 2, color: "white", ml: 5 }}>
+                  Restaurantes
                 </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              </Link>
+
+              <Link component={RouterLink} to="/admin/restaurantes/novo">
+                <Button sx={{ my: 2, color: "white", ml: 5 }}>
+                  Novo Restaurante
+                </Button>
+              </Link>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Box>
+        <Container maxWidth="lg" sx={{ mt: 1 }}>
+          <Paper sx={{ p: 2 }}>
+            {/* conteudo da pagina */}
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Nome</TableCell>
+                    <TableCell>Editar</TableCell>
+                    <TableCell>Excluir</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {restaurantes.map((restaurante) => (
+                    <TableRow key={restaurante.id}>
+                      <TableCell>{restaurante.nome}</TableCell>
+                      <TableCell>
+                        [{" "}
+                        <RouterLink to={`/admin/restaurantes/${restaurante.id}`}>
+                          Editar
+                        </RouterLink>{" "}
+                        ]
+                      </TableCell>
+                      <TableCell>
+                        <Button
+                          variant="outlined"
+                          color="error"
+                          onClick={() => excluir(restaurante)}
+                        >
+                          Excluir
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </Container>
+      </Box>
+    </>
   );
 };
 
