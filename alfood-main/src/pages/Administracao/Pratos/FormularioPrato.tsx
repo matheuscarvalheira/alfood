b0b75762ec1402a4ce1718230a3bd1b81e383bf4
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-09-15 16:22:17 -0300

    arranging the dish form

--- alfood-main/src/pages/Administracao/Pratos/FormularioPrato.tsx
+++ alfood-main/src/pages/Administracao/Pratos/FormularioPrato.tsx
@@ -36,17 +36,46 @@ export default function FormularioPrato() {
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("nome", nomePrato);
+    formData.append("descricao", descricao);
+
+    formData.append("tag", tag);
+
+    formData.append("restaurante", restaurante);
+
+    if (imagem) {
+      formData.append("imagem", imagem);
+    }
+
+    http.request({
+      url: 'pratos/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      data: formData
+    })
+    .then(() => {
+      setDescricao('')
+      setNomePrato('')
+      setTag('')
+      setRestaurante('')
+      alert('Prato cadastrado com sucesso! ')
+    })
+    .catch(erro => console.log(erro))
+
   };
 
   const selecionarArquivo = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files?.length) {
-      setImagem(event.target.files[0])
-    } else{
-      setImagem(null)
+      setImagem(event.target.files[0]);
+    } else {
+      setImagem(null);
     }
-
-
-  }
+  };
 
   return (
     <>
@@ -89,7 +118,7 @@ export default function FormularioPrato() {
               onChange={(evento) => setTag(evento.target.value)}
             >
               {tags.map((tag) => (
-                <MenuItem key={tag.id} value={tag.id}>
+                <MenuItem key={tag.id} value={tag.value}>
                   {tag.value}
                 </MenuItem>
               ))}
